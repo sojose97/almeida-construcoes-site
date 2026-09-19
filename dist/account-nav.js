a75a23a89b1255fe21fa9b48fb682c44657bbdef
sojose97 <sojose97@hotmail.com>
2026-09-19 16:05:31 -0300

    Use Almeida yellow for customer greeting

--- dist/account-nav.js
+++ dist/account-nav.js
@@ -14,7 +14,7 @@ Object.assign(account.querySelector('.account-symbol').style,{display:'grid',pla
 Object.assign(account.querySelector('.account-symbol svg').style,{width:'30px',height:'30px',fill:'#f1f1ed'});
 Object.assign(account.querySelector('.account-symbol-cutout').style,{fill:'#191919'});
 Object.assign(account.querySelector('.account-copy').style,{display:'flex',flexDirection:'column',gap:'3px'});
-Object.assign(greeting.style,{display:'block',color:'#ff3344',fontSize:'11px',lineHeight:'1',fontWeight:'900',whiteSpace:'nowrap',textTransform:'uppercase'});
+Object.assign(greeting.style,{display:'block',color:'#ffd026',fontSize:'11px',lineHeight:'1',fontWeight:'900',whiteSpace:'nowrap',textTransform:'uppercase'});
 
 account.onclick=()=>{location.href='conta.html'};
 
