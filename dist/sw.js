62de6fd0733782e8bd8f0cd62b79eec103022dd7
sojose97 <sojose97@hotmail.com>
2026-09-19 15:27:33 -0300

    Refresh service worker cache for account greeting

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE='almeida-catalogo-v40';
+const CACHE='almeida-catalogo-v41';
 const FILES=['./','index.html','conta.html','conta.js','produto.html','admin.html','style.css','site.js','admin.js','auth-storefront.js','account-nav.js','produto.js','image-cleanup.js','product-images.js','catalogo.js','supabase-config.js','assets/logo.jpg','assets/fachada-almeida.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)))});
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
